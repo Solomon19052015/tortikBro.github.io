c5d5c8f2dcc3a3a506113a28be21ecc5aafe30b4
Solomon19052015 <42998762+Solomon19052015@users.noreply.github.com>
2020-05-18 10:30:22 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -7,7 +7,7 @@ let priceEl = document.querySelector('.price');
 let totalPriceEl = document.querySelector(".totalPrice");
 let totalGramEl = document.querySelector(".totalGram");
 
-let arrPrice = 0;
+let totalPrice = 0;
 let totalGram = 0;
 let array = [] ;
 let resultSearch;
@@ -21,77 +21,121 @@ btn.addEventListener('click', inputValueGo);
 
 
 let namePriceProduct ={
-    "сливочное масло, 160" : 2.13,
-    "разрыхлитель, 15": 0.45,
-    "мука, 2000": 2.4,
+    "сливочное масло 82.5%, 160": 2.55,
+    "разрыхлитель, 15": 0.75,
+    "пшеничная мука, 2000": 2.4,
     "сода, 500": 1.00,
     "какао, 150": 1.77,
     "сахар, 1000": 1.54,
     "соль, 1000": 0.40,
-    "яица, 450":1.80,
-    "молоко, 900": 1.48,
+    "яица с1, 600": 1.80,
+    "молоко 3.2%, 900": 1.48,
+    "молоко 2.5%, 900": 1.15,
     "кофе растворимый, 250": 14.29,
-    "грецкий орех, 1000":21.45,
-    "желатин, 50": 1.50,
+    "грецкий орех, 1000": 21.45,
+    "желатин, 50": 1.19,
     "вода, 1000": 0.40,
     "апельсин, 1000": 3.00,
-    "крахмал, 450": 1.60,
-    "сливки 33%, 500": 6.10,
-    "молочный шоколад, 100":2.70,
+    "кукурузный крахмал, 450": 1.60,
+    "сливки 33%, 500": 6,
+    "шоколад горький 56%, 90": 1.29,
+    "молочный шоколад, 100": 1.35,
     "темный шоколад, 90": 2.25,
     "творожный сыр, 380": 5.00,
-    "сахарная пудра, 356": 1.25,
-    "растительное масло, 800": 2.30,
-    "вишня, 400":4.15,
-    "маскарпоне, 250": 5.25,
-    "ванилин, 2": 0.21,
-    "клубника, 400": 4.29,
-    "лимоный сок, 1000": 7.00,
+    "сахарная пудра, 350": 1.25,
+    "замороженная вишня, 400": 4.15,
+    "сыр маскарпоне, 250": 5.25,
+    "ванилин, 2": 0.29,
+    "замороженная клубника, 400": 4.29,
     "белый шоколад, 90": 2.00,
     "сливочный сыр, 400": 7.69,
-    "сливки 10%, 350": 2.05,
+    "сливки 10%, 500": 1.75,
     "сливки 22%, 500": 4.70,
     "фундук, 1000": 33,
     "подсолнечное масло, 1000": 3.20,
     "сгущенное молоко, 380": 2.75,
     "сгущенное молоко(варёное), 360": 2.85,
     "чернослив, 1000": 13,
-    "крахмал кукурузный, 450": 1.60,
-    "печенье шоколадное, 80": 2.69,
+    "крахмал картофельный, 500": 2.19,
+    "печенье шоколадное, 450": 2.75,
     "мак, 100": 3.85,
-    "лимон, 1000": 7.70,
+    "лимон, 1000": 7.00,
     "лимонная кислота, 30": 0.75,
     "миндальная мука, 100": 12,
-    "кофе молотый, 250": 5,
-
-    
-
-
+    "кофе нерастворимый, 250": 5,
+    "консервированные персики половинки, 800": 3.79,
+    "банан, 1000": 3,
+    "мед, 200": 2.89,
+    "сметана 15%, 400": 1.75,
+    "сметана 10%, 350": 2.09,
+    "сметана 20%, 400": 2.09,
+    "сметана 25%, 400": 2.45,
+    "сметана 26%, 400": 2.65,
+    "сметана 18%, 400": 2.06,
+    "сметана 22%, 400": 2.29,
+    "глюкозный сироп, 500": 4.35,
+    "агар агар, 900": 4.70,
+    "замороженная черника, 400": 8.29,
+    "замороженная малина, 450": 7.45,
+    "замороженная смородина, 300": 2.39,
+    "уксус 9%, 1000": 1.29,
+    "уксус винный 6%, 1000": 6.89,
+    "уксус яблочный 6%, 250": 2.60,
+    "лайм, 1000": 5.49,
+    "лимонный джем, 300": 7.09,
+    "кефир 2.5%, 950": 1.75,
+    "кефир 1.5%, 950": 1.69,
+    "кефир 3.2%, 930": 1.65,
+    "арахис жаренный соленый, 1000": 7.69,
+    "батончик сникерс, 95": 1.65,
+    "киви, 1000": 6.59,
+    "печенье орео, 228": 3.29,
+    "молотая корица, 15": 0.76,
+    "морковь, 1000": 0.65,
+    "изюм светлый, 1000": 12.25,
+    "кокосовая стружка, 20": 0.69,
+    "мускатный орех, 15": 2.09,
+    "фисташки, 1000": 37.99,
+    "кедровый орех, 1000": 100.99,
+    "курага, 1000": 6.59,
+    "финики, 1000": 5.49,
+    "творог 5%, 220": 1.65,
+    "творог 1%, 220": 1.51,
+    "творог 9%, 220": 1.74,
+    "имбирь молотый, 20": 1.19,
+    "сыр мягкий рикотта 40%, 500": 4.05,
+    "консервированный ананас кольца, 565": 3.09,
+    "консервированный ананас кусочки, 565": 2.59,
+    "сухие быстродействующие дрожжи, 11": 0.43,
 
 }
 
 //СОБЫТИЕ НАЖАТИЯ КНОПКИ
 
 function inputValueGo(){
-    
+ 
     inputValue = input.value;
-   // console.log(inputValue);
-  array= processingTextAddArray(inputValue);
+ // console.log(inputValue.length);
+    if(inputValue.length == 0){
+        return;
+    }
+     array= processingTextAddArray(inputValue);
   resultSearch = saerchValueObject(namePriceProduct, array[0]);
-  resultCalculation = calculationPrice(array[1],resultSearch);
+  resultCalculation = Number(calculationPrice(array[1],resultSearch));
+console.log(resultCalculation + " " + typeof resultCalculation);
   savePriceOnArray.push(resultCalculation);
-
   saveGramOnArray.push(array[1]);
   
-  arrPrice += Number(resultCalculation);
-  totalGram+=Number(array[1]);
- // console.log(totalGram);
-  if(arrPrice>0){
-    arrPrice = Number(arrPrice.toFixed(2));
-  }
+  totalPrice = Number(totalPrice)+  Number(resultCalculation);
+  totalPrice = totalPrice.toFixed(2);
+  console.log(totalPrice + " " + typeof totalPrice);
+  totalGram= Number(totalGram) + Number(array[1]);
+ /*  if(totalPrice>0){
+    totalPrice = Number(totalPrice.toFixed(2));
+  } */
   
  addHtml(array[0],array[1],resultCalculation);
-  addHtmlTotalPrice(totalPriceEl,arrPrice);
+  addHtmlTotalPrice(totalPriceEl,totalPrice);
   addHtmlTotalGram(totalGramEl,totalGram);
 
 setTimeout(allElPp,50);
@@ -110,10 +154,14 @@ document.onkeyup = function (e) {
 	e = e || window.event;
 	if (e.keyCode === 13) {
 		inputValueGo();
-	}
+    }
+    else if(e.keyCode == 46){
+        clearListFunctionEvent();
+    }
 	// Отменяем действие браузера
 	return false;
 }
+
 
 
 //С ФОРМЫ ЗНАЧЕНИЕ РАЗДЕЛЯЕТСЯ И ДОБАВЛЯЕТСЯ В МАССИВ
@@ -121,10 +169,14 @@ function processingTextAddArray(value){
     
     let indx = value.indexOf(',',1);
     let valueNameProduct = value.slice(0,indx).toLowerCase();
-  
+ 
     let valueGram = value.slice(indx+1, value.lenght).trim();
-    if(valueNameProduct == "яица"){
-        valueGram*=45;
+    if(valueNameProduct == "яица с1"){
+        valueGram*=60;
+    }
+    if(isNaN(valueGram) ){
+     
+        return;
     }
     let arr = [];
     arr.push(valueNameProduct);
@@ -198,11 +250,15 @@ function addClassAnimation(el){
 }
 
 function addHtmlTotalPrice(el,val){
+    el.innerHTML = '';
+    console.log(` цена ${val}`);
     el.innerHTML =  val + "р";
 
 }
 
 function addHtmlTotalGram(el,val){
+    el.innerHTML = '';
+    console.log(` граммы ${val}`);
     el.innerHTML =   val + "г";
 
 }
@@ -217,50 +273,49 @@ function allElPp(){
 }
 allElPp();
 
+//СОБЫТИЕ УДЛАЕНИЕ СТРОКИ СПИСКА
 function clickDelete(){
     let delPrice = searchEventCkick();
-   
-  
-  //  (arrPrice-=searchEventCkick()).toFixed(2);
-    arrPrice-=delPrice[0];
-   // console.log(delPrice[0]);
-    totalGram-=Number(delPrice[1]);
+     totalPrice-=delPrice[0];
+     totalGram-=Number(delPrice[1]);
 
-    if(arrPrice>0){
-        arrPrice= Number(arrPrice.toFixed(2));
+    if(totalPrice>0){
+        totalPrice= Number(totalPrice.toFixed(2));
     }
-    addHtmlTotalPrice(totalPriceEl,arrPrice);
+    addHtmlTotalPrice(totalPriceEl,totalPrice);
     addHtmlTotalGram(totalGramEl,totalGram);
   }
 
+  //ОПРЕДЕЛНИЕ СТРОКИ СПИСКА ДЛЯ УДАЛЕНИЯ
 function searchEventCkick(){
     let elEvent =  event.target;
     let needClass = elEvent.classList[0];
     let allEventEl = document.querySelectorAll("." + needClass);
-    console.log(allEventEl);
+    //console.log(allEventEl);
      let decreaseSum=decreaseTotalPrice(needClass);
    let decreaseGram= decreaseTotalGram(needClass);
-   let arrPriceGram=[] ;
-   arrPriceGram.push(decreaseSum);
-   arrPriceGram.push(decreaseGram);
+   let totalPriceGram=[] ;
+   totalPriceGram.push(decreaseSum);
+   totalPriceGram.push(decreaseGram);
    
       for(let i = 0; i < allEventEl.length; i++){
         allEventEl[i].classList.add('delString');
     }
 
     setTimeout(animationDelEl,700,allEventEl);
-       return arrPriceGram;
+       return totalPriceGram;
 }
 
-
+//ПЕРЕРСЧЕТ ИТОГОВОЙ ЦЕНЫ ПОСЛЕ УДАЛЕНИЯ СТРОКИ СПИСКА
 function decreaseTotalPrice(el){
     let num = el.slice(1, el.lenght);
     let classPriceNeed = document.querySelector(".price" + num);
-    console.log(classPriceNeed);
+   // console.log(classPriceNeed);
     let contentPrice = classPriceNeed.textContent;
     return Number(contentPrice);
 }
 
+//ПЕРЕРСЧЕТ ИТОГОВОЙ ГРАММОВКИ ПОСЛЕ УДАЛЕНИЯ СТРОКИ СПИСКА
 function decreaseTotalGram(el){
     let num = el.slice(1, el.lenght);
     let classPriceNeed = document.querySelector(".gram" + num);
@@ -292,6 +347,10 @@ helpWindowEl.addEventListener('click', closeHelpWindowFunction) ;
 function openHelpWindowFunction(){
     countHelp++;
      helpWindowEl.classList.add('openHelpWindow');
+     if(countHelp == 2){
+        helpWindowEl.classList.remove('openHelpWindow');
+        countHelp = 0;
+     }
    
 }
 
@@ -303,7 +362,7 @@ function closeHelpWindowFunction(){
 
 
 
-
+//ПЕРЕРАСЧЕТ ОТ РАЗМЕРА ФОРМЫ
 
 let sizeForm = document.querySelector(".sizeFormCake");
 let lastSizeFormCake = sizeForm.value;
@@ -324,21 +383,21 @@ function eventRecalculation(){
     let factor = calculationFactor(lastSizeFormCake, nowSizeCake);
     savePriceOnArray= recalculationValue(savePriceOnArray,factor,"price");
     saveGramOnArray = recalculationValue(saveGramOnArray,factor,"gram");
-    
-    arrPrice= sumAfterRecaculation(savePriceOnArray);
+   
+    console.log(savePriceOnArray);
+    totalPrice= sumAfterRecaculation(savePriceOnArray);
     totalGram= sumAfterRecaculation(saveGramOnArray);
     
-    if(arrPrice !=undefined && totalGram !=undefined){
-        console.log(arrPrice,totalGram)
-    arrPrice = arrPrice.toFixed(2);
+    if(totalPrice !=undefined && totalGram !=undefined){
+     totalPrice = totalPrice.toFixed(2);
     totalGram = totalGram.toFixed(0);
     }
     else{
-        arrPrice = 0;
+        totalPrice = 0;
         totalGram = 0;
     }
 
-    addHtmlTotalPrice(totalPriceEl,arrPrice);
+    addHtmlTotalPrice(totalPriceEl,totalPrice);
     addHtmlTotalGram(totalGramEl,totalGram);
  
     lastSizeFormCake=nowSizeCake;
@@ -347,7 +406,7 @@ function eventRecalculation(){
 }
 
 
-
+//ОСНОВНОЙ ПЕРЕРАСЧЕТ КАЖДОГО ПУНКТА СПИСКА ПО ИЗМЕНЕНИЮ ФОРМЫ 
 function recalculationValue(arr, factor,name){
       
     for(let i = 0; i < arr.length; i++){
@@ -357,7 +416,7 @@ function recalculationValue(arr, factor,name){
             if(name == "gram"){
                 arr[i] = (arr[i] * factor).toFixed(0);
                 arr[i] = Number(arr[i]);
-                console.log(arr);
+               // console.log(arr);
             }
             else{
                 arr[i] = (arr[i] * factor).toFixed(2);
@@ -372,7 +431,7 @@ function recalculationValue(arr, factor,name){
            
             if(name == "gram"){
                 arr[i] = (arr[i] / factor).toFixed(0);
-                console.log(arr[i] , factor);
+              //  console.log(arr[i] , factor);
             }
             else{
                 arr[i] = (arr[i] / factor).toFixed(2);
@@ -381,6 +440,7 @@ function recalculationValue(arr, factor,name){
             searchTagRecalculation(i,name,arr[i]);
         }
     }
+    console.log(arr);
     return arr
 }
 
@@ -388,14 +448,13 @@ function recalculationValue(arr, factor,name){
 
 function searchTagRecalculation(index, name,value){
     let nameClass ='.'+ name + index;
-    console.log(nameClass);
-
     let el =document.querySelector(nameClass);
     el.innerHTML=value;
   
     return;
 }
 
+//ПЕРЕРАСЧЕТ ИТОГОВОЙ СУММЫ ПОСЛЕ ИЗМЕНЕНИЯ РАЗМЕРА ФОРМЫ
 function sumAfterRecaculation(arr){
   
     let sum = 0;
@@ -405,15 +464,17 @@ function sumAfterRecaculation(arr){
         }
     }
     if(sum ==0){
-        return
+        return;
     }
     else{
-        console.log(sum);
+      //  console.log(sum);
+    
         return sum;
     }
    
 }
 
+//РАСЧЕТ КОЭФФИЦИЕНТА ДЛЯ РАЗМЕРА ФОРМЫ
 function calculationFactor(valLastSize, valNowSize){
   
     if(valNowSize>valLastSize){
@@ -426,3 +487,51 @@ function calculationFactor(valLastSize, valNowSize){
 
 }
 
+
+
+//ОЧИСКТА СПИСКА
+
+let clearListElement = document.querySelector('.clearList');
+
+clearListElement.addEventListener('click', clearListFunctionEvent);
+
+function clearListFunctionEvent(){
+    if(totalPrice == 0){
+        return;
+    }
+     clearListHTML();
+    clearArray(savePriceOnArray);
+    clearArray(saveGramOnArray);
+    totalPrice= cleatTotalValue(totalPrice);
+    totalGram=  cleatTotalValue(totalGram);
+    addHtmlTotalPrice(totalPriceEl,0);
+    addHtmlTotalPrice(totalGramEl,0);
+    countClass = -1;
+ 
+}
+
+function clearArray(arr){
+  
+    for(let i = arr.length; i > 0; i--){
+            arr.pop();
+    }
+    return;
+}
+
+function clearListHTML(){
+    let allElementList = document.querySelectorAll(".pp");
+   
+    for(let i = 0; i < allElementList.length; i++){
+        allElementList[i].classList.add('delString');
+
+        setTimeout(()=>{
+            allElementList[i].remove();
+        },700);
+      
+    }
+    return;
+}
+
+function cleatTotalValue(val){
+    return val = 0;
+}
